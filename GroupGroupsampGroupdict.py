a752ac3d74890a48a1188c359a6546f05957c128
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2024-04-08 12:18:34 +0530

    Add files via upload

---
+++ GroupGroupsampGroupdict.py
@@ -0,0 +1,14 @@
+"""
+Title     : Group(), Groups() &amp; Groupdict()
+Subdomain : Regex and Parsing
+Domain    : Python
+"""
+
+import re
+
+s = input()
+res = re.search(r"([A-Za-z0-9])\1", s)
+if res is None:
+    print(-1)
+else:
+    print(res[1])
